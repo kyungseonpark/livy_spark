5f11420bf92927481b8b6e7e0ff69dd55d49a22f
kyungseonpark <kyungseon.park@aizen.co>
2023-09-08 18:03:37 +0900

    add build.sh

--- build.sh
+++ build.sh
@@ -36,18 +36,24 @@ if [ ! -d "$built_dir" ]; then
     echo "디렉토리를 생성했습니다."
 fi
 
-built_jars="$(built_dir)/jars"
-#built_rcs_jars="$(built_dir)/rcs-jars"
-built_repl_jars="$(built_dir)/repl_2.12-jars"
+built_jars="$built_dir/jars"
+built_rcs_jars="$built_dir/rsc-jars"
+built_repl_jars="$built_dir/repl_2.12-jars"
 
-cp "$(livy_dir)/DISCLAIMER" "$(built_dir)/"
-cp "$(livy_dir)/LICENSE" "$(built_dir)/"
-cp "$(livy_dir)/NOTICE" "$(built_dir)/"
-cp -rf "$(livy_dir)/bin" "$(built_dir)/"
-cp -rf "$(livy_dir)/conf" "$(built_dir)/"
+cp "$livy_dir/DISCLAIMER" "$built_dir/"
+cp "$livy_dir/LICENSE" "$built_dir/"
+cp "$livy_dir/NOTICE" "$built_dir/"
+cp -rf "$livy_dir/bin" "$built_dir/"
+cp -rf "$livy_dir/conf" "$built_dir/"
 
+mkdir "$built_jars"
 find "$livy_dir" -name "*.jar" -exec cp {} "$built_jars" \;
 
-#cp -rf "$(livy_dir)/conf" "$built_rcs_jars"
-cp -rf "$(livy_dir)/repl/scala-2.12/target/jars/" "$built_repl_jars"
+mkdir "$built_repl_jars"
+cp -rf "$livy_dir/repl/scala-2.12/target/jars/" "$built_repl_jars"
 
+mkdir "$built_rcs_jars"
+find "$built_jars" -name "*livy-api*.jar" -exec cp {} "$built_rcs_jars" \;
+find "$built_jars" -name "*livy-rsc*.jar" -exec cp {} "$built_rcs_jars" \;
+find "$built_jars" -name "*livy-thriftserver*.jar" -exec cp {} "$built_rcs_jars" \;
+find "$built_jars" -name "*netty-all*.jar" -exec cp {} "$built_rcs_jars" \;
